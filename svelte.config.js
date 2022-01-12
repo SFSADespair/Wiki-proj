cb99656daa3d4093d5e5f326ccf58fc5fcce62cc
SFSADespair <68440041+SFSADespair@users.noreply.github.com>
2022-01-12 17:37:20 +0200

    Add files via upload

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
+import adapter from '@sveltejs/adapter-auto';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    kit: {
+        adapter: adapter(),
+
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte'
+    }
 };
 
 export default config;
