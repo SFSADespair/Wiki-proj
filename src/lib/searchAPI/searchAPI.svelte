288dc543b0534d7411225af481011c77eaa32eb5
SFSADespair <tyrell.snyders@gmail.com>
2022-01-12 17:48:12 +0200

    Removed fa

--- src/lib/searchAPI/searchAPI.svelte
+++ src/lib/searchAPI/searchAPI.svelte
@@ -2,8 +2,7 @@
     let id;
     let artSearch;
     
-    import Fa from 'svelte-fa'
-    import { faSearch } from '@fortawesome/free-solid-svg-icons'
+    
 </script>
 
 <div class="container div_content">
@@ -11,7 +10,7 @@
         <form action="/articles/article/{artSearch}" method="get">
             <input type="text" placeholder="Search" name="inpSearch" bind:value="{artSearch}" />
             <!-- {artSearch.toUpperCase()} -->
-            <button type="submit" class="btn btn-success"><Fa icon={faSearch} size="md" /></button>
+            <button type="submit" class="btn btn-success">Search</button>
         </form>
     </div>
 </div>
